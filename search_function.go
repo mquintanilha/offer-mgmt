279dd369370f5700d68a8250e555888eeec8e783
Marcos Quintanilha <marcos.quintanilha@6bank.com>
2020-01-15 18:11:25 -0300

    Parse json function.

--- search_function.go
+++ search_function.go
@@ -2,29 +2,87 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"io/ioutil"
+	"encoding/json"
 )
+
+// Data Structures
+
+type Track struct {
+	Top_traks	[]Track_info
+}
+
+type Track_info struct {
+    Info       []Request_info
+	Pag   	   []Pagination
+	Data       []Offers
+}
+
+type Request_info struct {
+	Status	string `json:"status"`
+	Message	string `json:"message"`
+}
+
+type Pagination struct {
+	Page		string `json:"page"`
+	Size		string `json:"size"`
+	Total_Size	string `json:"totalSize"`
+	Total_Page	string `json:"totalPage"`
+}
+
+type Offers struct {
+	Data_feed	[]Feed
+}
+
+type Feed struct {
+	Id			string `json:"id"`
+	Name		string `json:"name"`
+	Category	string `json:"category"`
+	Link		string `json:"link"`
+	Thumbnail	string `json:"thumbnail"`
+	Price		string `json:"price"`
+	Discount	string `json:"discount"`
+	Installment	string `json:"installment"`
+	Store		string `json:"store"`
+}
 
 func main() {
 
-	url := "http://sandbox-api.lomadee.com/v3/{{ token }}/offer/_search?keyword={{ product }}&sourceId={{ sourd_id }}"
+	const jsonData = `{
+		"requestInfo": {
+			"status": "",
+			"message": ""
+		},
+		"pagination": {
+			"page": 0,
+			"size": 0,
+			"totalSize": 0,
+			"totalPage": 0
+		},
+		"offers": [
+			{
+				"id": "234512",
+				"name": "Celular",
+				"category": {},
+				"link": "",
+				"thumbnail": "",
+				"price": 0,
+				"discount": 0,
+				"installment": {},
+				"store": {}
+			}
+		]
 
-	req, _ := http.NewRequest("GET", url, nil)
+	}`
 
-	req.Header.Add("Accept", "*/*")
-	req.Header.Add("Cache-Control", "no-cache")
-	req.Header.Add("Host", "sandbox-api.lomadee.com")
-	req.Header.Add("Accept-Encoding", "gzip, deflate")
-	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("cache-control", "no-cache")
+	data := []byte(jsonData)
 
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	fmt.Println(res)
-	fmt.Println(string(body))
+	offers_day := Offers.Feed{}
+	err := json.Unmarshal(data, &offers_day)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	// fmt.Printf(offers_day.Id, offers_day.Name)
+	fmt.Printf("Id: %+v, Name: %+v", offers_day.Id, offers_day.Name)
 
 }
