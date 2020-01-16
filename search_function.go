15a0b0547e183dbb41364df4d56206fa8a9ad729
Marcos Quintanilha <marcos.quintanilha@6bank.com>
2020-01-16 12:56:34 -0300

    Create an simple web service in Go.

--- search_function.go
+++ search_function.go
@@ -10,13 +10,13 @@ import (
 // Data Structures
 
 type Track struct {
-	Top_traks	[]Track_info
+	Top_traks	Track_info
 }
 
 type Track_info struct {
-    Info       []Request_info
-	Pag   	   []Pagination
-	Data       []Offers
+    Info       Request_info
+	Pag   	   Pagination
+	Data       Offers
 }
 
 type Request_info struct {
@@ -25,14 +25,14 @@ type Request_info struct {
 }
 
 type Pagination struct {
-	Page		string `json:"page"`
-	Size		string `json:"size"`
-	Total_Size	string `json:"totalSize"`
-	Total_Page	string `json:"totalPage"`
+	Page		int  `json:"page"`
+	Size		int  `json:"size"`
+	Total_Size	int  `json:"totalSize"`
+	Total_Page	int  `json:"totalPage"`
 }
 
 type Offers struct {
-	Data_feed	[]Feed
+	Data_feed	Feed
 }
 
 type Feed struct {
@@ -41,61 +41,49 @@ type Feed struct {
 	Category	string       `json:"category"`
 	Link		string 	     `json:"link"`
 	Thumbnail	string       `json:"thumbnail"`
-	Price		float64      `json:"price"`
-	Discount	float64      `json:"discount"`
+	Price		float32      `json:"price"`
+	Discount	float32      `json:"discount"`
 	Installment	string       `json:"installment"`
 	Store		string       `json:"store"`
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 
-	// const jsonData = `{
-	// 	"requestInfo": {
-	// 		"status": "",
-	// 		"message": ""
-	// 	},
-	// 	"pagination": {
-	// 		"page": 0,
-	// 		"size": 0,
-	// 		"totalSize": 0,
-	// 		"totalPage": 0
-	// 	},
-	// 	"offers": [
-	// 		{
-	// 			"id": "234512",
-	// 			"name": "Celular",
-	// 			"category": {},
-	// 			"link": "",
-	// 			"thumbnail": "",
-	// 			"price": 0,
-	// 			"discount": 0,
-	// 			"installment": {},
-	// 			"store": {}
-	// 		}
-	// 	]
-
-	// }`
-	// title := r.URL.Path[len("/offer/"):]
-	// p, _ := loadPage(title)
-	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-
 	const jsonData = `{
-		"id": "234512",
-		"name": "Celular",
-		"link": "",
-		"thumbnail": "",
-		"price": 10.0,
-		"discount": 3.0
+		"requestInfo": {
+			"status": "",
+			"message": ""
+		},
+		"pagination": {
+			"page": 0,
+			"size": 0,
+			"totalSize": 0,
+			"totalPage": 0
+		},
+		"offers": [
+			{
+				"id": "234512",
+				"name": "Celular",
+				"category": {},
+				"link": "",
+				"thumbnail": "",
+				"price": 0,
+				"discount": 0,
+				"installment": {},
+				"store": {}
+			}
+		]
+
 	}`
 
     data := []byte(jsonData)
-	offers_day := Feed{}
+	offers_day := Track{}
 	err := json.Unmarshal(data, &offers_day)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-	fmt.Printf("Id: %s, Name: %s, Prince: %s, Link: %s, Discount: %s", offers_day.Id, offers_day.Name, offers_day.Price, offers_day.Discount)
+	fmt.Printf("Id: %v", offers_day.Top_traks.Data.Data_feed.Id)
 
 }
 
