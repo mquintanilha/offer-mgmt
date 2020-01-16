dc814195854a6b05d7d6011a8aca67dcd055f2af
Marcos Quintanilha <marcos.quintanilha@6bank.com>
2020-01-15 22:32:11 -0300

    Partial parse json data.

--- search_function.go
+++ search_function.go
@@ -34,55 +34,63 @@ type Offers struct {
 }
 
 type Feed struct {
-	Id			string `json:"id"`
-	Name		string `json:"name"`
-	Category	string `json:"category"`
-	Link		string `json:"link"`
-	Thumbnail	string `json:"thumbnail"`
-	Price		string `json:"price"`
-	Discount	string `json:"discount"`
-	Installment	string `json:"installment"`
-	Store		string `json:"store"`
+	Id			string       `json:"id"`
+	Name		string       `json:"name"`
+	Category	string       `json:"category"`
+	Link		string 	     `json:"link"`
+	Thumbnail	string       `json:"thumbnail"`
+	Price		float64      `json:"price"`
+	Discount	float64      `json:"discount"`
+	Installment	string       `json:"installment"`
+	Store		string       `json:"store"`
 }
 
 func main() {
 
-	const jsonData = `{
-		"requestInfo": {
-			"status": "",
-			"message": ""
-		},
-		"pagination": {
-			"page": 0,
-			"size": 0,
-			"totalSize": 0,
-			"totalPage": 0
-		},
-		"offers": [
-			{
-				"id": "234512",
-				"name": "Celular",
-				"category": {},
-				"link": "",
-				"thumbnail": "",
-				"price": 0,
-				"discount": 0,
-				"installment": {},
-				"store": {}
-			}
-		]
+	// const jsonData = `{
+	// 	"requestInfo": {
+	// 		"status": "",
+	// 		"message": ""
+	// 	},
+	// 	"pagination": {
+	// 		"page": 0,
+	// 		"size": 0,
+	// 		"totalSize": 0,
+	// 		"totalPage": 0
+	// 	},
+	// 	"offers": [
+	// 		{
+	// 			"id": "234512",
+	// 			"name": "Celular",
+	// 			"category": {},
+	// 			"link": "",
+	// 			"thumbnail": "",
+	// 			"price": 0,
+	// 			"discount": 0,
+	// 			"installment": {},
+	// 			"store": {}
+	// 		}
+	// 	]
 
+	// }`
+
+	const jsonData = `{
+		"id": "234512",
+		"name": "Celular",
+		"link": "",
+		"thumbnail": "",
+		"price": 10.0,
+		"discount": 3.0
 	}`
 
 	data := []byte(jsonData)
 
-	offers_day := Offers.Feed{}
+	offers_day := Feed{}
 	err := json.Unmarshal(data, &offers_day)
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-	// fmt.Printf(offers_day.Id, offers_day.Name)
-	fmt.Printf("Id: %+v, Name: %+v", offers_day.Id, offers_day.Name)
+	fmt.Printf("Id: %s, Name: %s, Prince: %s, Discount: %s", offers_day.Id, offers_day.Name, offers_day.Price, offers_day.Discount)
 
 }
