f73bc437225dfac9c9171b5509158bd3ff744391
Marcos Quintanilha <marcos.quintanilha@6bank.com>
2020-01-16 23:46:55 -0300

    Data structure update.

--- search_function.go
+++ search_function.go
@@ -9,81 +9,75 @@ import (
 
 // Data Structures
 
-type Track struct {
-	Top_traks	Track_info
-}
-
-type Track_info struct {
-    Info       Request_info
-	Pag   	   Pagination
-	Data       Offers
-}
-
-type Request_info struct {
-	Status	string `json:"status"`
-	Message	string `json:"message"`
-}
-
-type Pagination struct {
-	Page		int  `json:"page"`
-	Size		int  `json:"size"`
-	Total_Size	int  `json:"totalSize"`
-	Total_Page	int  `json:"totalPage"`
-}
-
 type Offers struct {
 	Data_feed	Feed
 }
 
 type Feed struct {
-	Id			string       `json:"id"`
+
+	Category	Category
 	Name		string       `json:"name"`
-	Category	string       `json:"category"`
+	Price_From	float32		 `json:"priceFrom"`
+	Price		float32      `json:"price"`
+	Installment	Installment
+	Id			string       `json:"id"`
 	Link		string 	     `json:"link"`
 	Thumbnail	string       `json:"thumbnail"`
-	Price		float32      `json:"price"`
-	Discount	float32      `json:"discount"`
-	Installment	string       `json:"installment"`
-	Store		string       `json:"store"`
+	Store		Store
+}
+
+type Category struct {
+	Link	string
+	Id		int
+	Name	string
+}
+
+type Installment struct {
+	Value 		float32
+	Quantity 	int
+}
+
+type Store struct {
+	Thumbnail	string		`json:"thumbnail"`
+	Link 		string		`json:"link"`
+	Id			int			`json:"id"`
+	Name		string		`json:"name"`
+
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 
-	const jsonData = `{
-		"requestInfo": {
-			"status": "",
-			"message": ""
+	jsonData := `{
+		"category": {
+			"link": "http://api.lomadee.com/v3/1578677161277ae2a08a2/category/_id/0?sourceId=22830830",
+			"id": 0,
+			"name": "Geral"
 		},
-		"pagination": {
-			"page": 0,
-			"size": 0,
-			"totalSize": 0,
-			"totalPage": 0
+		"name": "Rele Auxiliar 24v 5 Terminais 0a 0a Vdo Cod.ref. D09308 Volvo /iveco",
+		"priceFrom": 29.25,
+		"price": 29.25,
+		"installment": {
+			"value": 29.25,
+			"quantity": 1
 		},
-		"offers": [
-			{
-				"id": "234512",
-				"name": "Celular",
-				"category": {},
-				"link": "",
-				"thumbnail": "",
-				"price": 0,
-				"discount": 0,
-				"installment": {},
-				"store": {}
-			}
-		]
-
+		"id": "62778060",
+		"link": "https://developer.lomadee.com/redir/validation/?sourceId=22830830&appToken=1578677161277ae2a08a2",
+		"thumbnail": "https://images-americanas.b2w.io/produtos/01/00/oferta/62778/0/62778063P1.jpg",
+		"store": {
+			"thumbnail": "https://www.lomadee.com/programas/BR/5632/imagemBox_80x60.png",
+			"link": "https://developer.lomadee.com/redir/validation/?sourceId=22830830&appToken=1578677161277ae2a08a2",
+			"id": 5632,
+			"name": "Americanas.com"
+		}
 	}`
 
-    data := []byte(jsonData)
-	offers_day := Track{}
-	err := json.Unmarshal(data, &offers_day)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-	fmt.Printf("Id: %v", offers_day.Top_traks.Data.Data_feed.Id)
+	offers_day := Feed{}
+	err := json.Unmarshal([]byte(jsonData), &offers_day)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Nome:%s, Quantity:%+v, Value:%+v, Loja:%s", offers_day.Name, offers_day.Installment.Quantity, offers_day.Installment.Value, offers_day.Store.Name)
 
 }
 
