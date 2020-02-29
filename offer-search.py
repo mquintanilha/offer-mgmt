0cfedced03e5843470aa11ad6e3c9bbcd6d85efc
Marcos Quintanilha <marcos.quintanilha@6bank.com>
2020-02-29 09:15:58 -0300

    New conditional for choose envidronment.

--- offer-search.py
+++ offer-search.py
@@ -3,20 +3,27 @@ import json
 import sys
 import csv
 
-querystring = {"sourceId":"<AFFILIATE_ID>"}
+querystring = {"sourceId":"22830830"}
 headers = {
     'Accept': "*/*",
     'Cache-Control': "no-cache",
-    'Host': "sandbox-api.lomadee.com",
+    'Host': "api.lomadee.com",
     'Accept-Encoding': "gzip, deflate",
     'Connection': "keep-alive",
     'cache-control': "no-cache"
     }
+host_sandbox = 'sandbox-api.lomadee.com'
+host_prod = 'api.lomadee.com'
 
-url = "http://sandbox-api.lomadee.com/v3/" + sys.argv[1] + "/offer/_search?keyword="+ sys.argv[2]
+if sys.argv[3] == 'prod':
+    url = "http://" + host_prod + "/v3/" + sys.argv[1] + "/offer/_search?keyword=" + sys.argv[2]
+elif sys.argv[3] == 'dev':
+    url = "http://" + host_sandbox + "/v3/" + sys.argv[1] + "/offer/_search?keyword=" + sys.argv[2]
+
+# url = "http://" + host + "/v3/" + sys.argv[1] + "/offer/_search?keyword=" + sys.argv[2]
 
 
-# Were SYS.ARG[1] is TOKEN APPLICATION and SYS.ARGV[2] is CATEGORY FOR SEARCH.
+# Were SYS.ARG[1] is TOKEN APPLICATION and SYS.ARGV[2] is CATEGORY FOR SEARCH and SYS.ARGV[3] is enviroment code in this case only prod or dev.
 
 response = requests.get(url, headers=headers, params=querystring)
 jsonRes = response.json()
@@ -25,8 +32,8 @@ with open('data.json', 'a') as outfile:
     json.dump(jsonRes['offers'], outfile, indent=4, separators=(',',':'))
 
 x = json.load(open('data.json', 'r'))
-
 y = {"installment": { "value": "in stock", "quantity": "new" }}
+z = {"g_id": 772}
 
 f = csv.writer(open('import.csv', 'wb+'))
 
@@ -44,13 +51,13 @@ f.writerow(['id',
 
 for x in x:
     f.writerow([x['id'],
-              x['name'],
-              x['name'],
-              y['installment']['value'],
-              y['installment']['quantity'],
-              x['price'],
-              x['link'],
-              x['thumbnail'],
-              x['store']['name'],
-              x['store']['thumbnail'],
-              x['id']])
+                x['name'],
+                x['name'],
+                y['installment']['value'],
+                y['installment']['quantity'],
+                x['price'],
+                x['store']['link'],
+                x['store']['thumbnail'],
+                x['store']['name'],
+                x['store']['thumbnail'],
+                z['g_id']])
