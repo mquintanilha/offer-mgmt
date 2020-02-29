cd4022f7e87de0228cc46a81a4f48a599e06d753
Marcos Quintanilha <marcos.quintanilha@6bank.com>
2020-02-29 09:28:24 -0300

    Update conditional, add for Host in header.

--- offer-search.py
+++ offer-search.py
@@ -3,25 +3,31 @@ import json
 import sys
 import csv
 
-querystring = {"sourceId":"22830830"}
-headers = {
-    'Accept': "*/*",
-    'Cache-Control': "no-cache",
-    'Host': "api.lomadee.com",
-    'Accept-Encoding': "gzip, deflate",
-    'Connection': "keep-alive",
-    'cache-control': "no-cache"
-    }
+querystring = {"sourceId":"<ID_AFILIADO>"}
+
 host_sandbox = 'sandbox-api.lomadee.com'
 host_prod = 'api.lomadee.com'
 
 if sys.argv[3] == 'prod':
     url = "http://" + host_prod + "/v3/" + sys.argv[1] + "/offer/_search?keyword=" + sys.argv[2]
+    headers = {
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Host': host_prod,
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
 elif sys.argv[3] == 'dev':
     url = "http://" + host_sandbox + "/v3/" + sys.argv[1] + "/offer/_search?keyword=" + sys.argv[2]
-
-# url = "http://" + host + "/v3/" + sys.argv[1] + "/offer/_search?keyword=" + sys.argv[2]
-
+    headers = {
+        'Accept': "*/*",
+        'Cache-Control': "no-cache",
+        'Host': host_sandbox,
+        'Accept-Encoding': "gzip, deflate",
+        'Connection': "keep-alive",
+        'cache-control': "no-cache"
+        }
 
 # Were SYS.ARG[1] is TOKEN APPLICATION and SYS.ARGV[2] is CATEGORY FOR SEARCH and SYS.ARGV[3] is enviroment code in this case only prod or dev.
 
@@ -32,7 +38,9 @@ with open('data.json', 'a') as outfile:
     json.dump(jsonRes['offers'], outfile, indent=4, separators=(',',':'))
 
 x = json.load(open('data.json', 'r'))
+
 y = {"installment": { "value": "in stock", "quantity": "new" }}
+
 z = {"g_id": 772}
 
 f = csv.writer(open('import.csv', 'wb+'))
